5f39af815e66d3633fb2b9e2207f822576643584
Diptanil Saha <diptanil.dev@gmail.com>
2024-10-17 12:34:09 +0530

    feat: book issue form

---
+++ app/transactions/forms.py
@@ -0,0 +1,30 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, EmailField
+from wtforms.validators import DataRequired, NumberRange, Email
+from wtforms.validators import ValidationError
+from app.db import db
+from app.books.models import BookStock, StockStatus
+from app.members.models import Member
+
+class BookIssueForm(FlaskForm):
+    stock_id = IntegerField('Stock ID', validators=[DataRequired(), NumberRange(min=1)])
+    member_email = EmailField('Member Email Address', validators=[DataRequired(), Email()])
+
+    def validate_stock_id(form, field):
+        stock: BookStock = db.session.get(BookStock, field.data)
+
+        if stock is None:
+            raise ValidationError('Stock not found.')
+        
+        if stock.status == StockStatus.TAKEN:
+            raise ValidationError('Stock is not available.')
+
+        
+    def validate_member_email(form, field):
+        member: Member = db.session.execute(db.select(Member).filter_by(email=field.data)).scalar_one_or_none()
+
+        if member is None:
+            raise ValidationError('Member not found.')
+        
+        if not member.is_allowed_to_rent_book:
+            raise ValidationError('Member is not allowed to rent book.')
