d0bc763b6f123cdf78bf8785d0357344ee61b3a4
Diptanil Saha <diptanil.dev@gmail.com>
2024-10-22 01:11:44 +0530

    feat: api url

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ class Config:
     DAILY_FINE = int(os.getenv('DAILY_FINE')) # In rupees
     NO_OF_BOOKS_RENTED_AT_A_TIME = int(os.getenv('NO_OF_BOOKS_RENTED_AT_A_TIME')) # Count of books can be rented at a time.
     PERMITTED_AMOUNT_OF_DUES = int(os.getenv('PERMITTED_AMOUNT_OF_DUES')) # In Rupees: Rs. 500 /-
+    API_URL = "https://frappe.io/api/method/frappe-library"
